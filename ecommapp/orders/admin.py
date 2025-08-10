67a138acfcdde931e971416026a113b9e86fe89b
Shreejan Maharjan <105190748+Ashura2003@users.noreply.github.com>
2025-08-10 15:38:38 +0545

    registered order model into django admin

--- ecommapp/orders/admin.py
+++ ecommapp/orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Order
+
+
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at', 'total_amount', 'payment_intent')
+    search_fields = ('user__username', 'payment_intent')
+    list_filter = ('created_at',)
